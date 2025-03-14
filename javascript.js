f5de7394febf750526cae9648be3541a69240048
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-14 15:04:56 +0100

    Add hover effect to the squares

--- javascript.js
+++ javascript.js
@@ -6,10 +6,18 @@ for(let i = 0; i < size ; i++){
     const row = document.createElement("div");
     row.classList.add("row");
     for(let j = 0; j < size ; j++){
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.add("square");
+        row.appendChild(squareDiv);
     }
     container.appendChild(row);
 }
 
+// Set a hover effect so that the grid divs change color 
+// when the mouse passes over them  
+const squares = document.querySelectorAll(".square");
+squares.forEach((square) =>{
+    square.addEventListener("mouseover", ()=>{
+        square.style.backgroundColor = "black";
+    });
+});
