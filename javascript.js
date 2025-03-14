1b2f551d55aad72f6ca8b4e38a13ba9888184cca
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-14 16:30:50 +0100

    Add button to set the grid size & align grid to center

--- javascript.js
+++ javascript.js
@@ -1,23 +1,47 @@
 const container = document.querySelector(".container");
-const size = 16;
+let size = 16;
 
 // Create a size*size grid of square divs
-for(let i = 0; i < size ; i++){
+function createGrid(){
+    const sqSize = 100 / size;
+    for(let i = 0; i < size ; i++){
     const row = document.createElement("div");
     row.classList.add("row");
     for(let j = 0; j < size ; j++){
         const squareDiv = document.createElement("div");
         squareDiv.classList.add("square");
+        squareDiv.style.width = `${sqSize}%`;
+        squareDiv.style.height = `${sqSize}%`;
         row.appendChild(squareDiv);
     }
     container.appendChild(row);
+    }
+    setHoverEffect();
 }
+
 
 // Set a hover effect so that the grid divs change color 
 // when the mouse passes over them  
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) =>{
-    square.addEventListener("mouseover", ()=>{
-        square.style.backgroundColor = "black";
+function setHoverEffect(){
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) =>{
+        square.addEventListener("mouseover", ()=>{
+            square.style.backgroundColor = "black";
+        });
     });
+}
+
+
+// Set click event on the button to change the size of the grid
+const button = document.querySelector(".button");
+button.addEventListener("click", ()=>{
+    let enterSize = prompt("Enter the number of squares per size (ex: 16 for 16x16)");
+    enterSize = parseInt(enterSize) || 16;
+    if(enterSize > 100) enterSize = 100;
+    setSize(enterSize);
+    createGrid();
 });
+
+function setSize(newSize){
+    size = newSize;
+}
